690a49e1bc5ef32e39ccc864c27693b653dd2f10
Arvan Ardana <113536365+arvardy184@users.noreply.github.com>
2025-06-26 21:49:19 +0700

    fix: package version upgrade

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,20 +12,6 @@ module.exports = {
     extend: {
       colors: {
         primary: {
-            //sementara
-          50: '#f0f9ff',
-          100: '#e0f2fe',
-          200: '#bae6fd',
-          300: '#7dd3fc',
-          400: '#38bdf8',
-          500: '#0ea5e9',
-          600: '#0284c7',
-          700: '#0369a1',
-          800: '#075985',
-          900: '#0c4a6e',
-        },
-        secondary: {
-            //sementara
           50: '#fdf2f8',
           100: '#fce7f3',
           200: '#fbcfe8',
@@ -36,6 +22,21 @@ module.exports = {
           700: '#be185d',
           800: '#9d174d',
           900: '#831843',
+            //sementara
+        
+        },
+        secondary: {
+            //sementara
+            50: '#f0f9ff',
+            100: '#e0f2fe',
+            200: '#bae6fd',
+            300: '#7dd3fc',
+            400: '#38bdf8',
+            500: '#0ea5e9',
+            600: '#0284c7',
+            700: '#0369a1',
+            800: '#075985',
+            900: '#0c4a6e',
         }
       },
       fontFamily: {
