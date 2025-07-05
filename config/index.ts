589ecc9696259ee4ee77c563be0900f3846238a9
zakkiislamii <mzislami05@gmail.com>
2025-07-05 23:58:43 +0700

    fix oauth from zakki branch

--- config/index.ts
+++ config/index.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = process.env.BASE_URL;
-export const GOOGLE_WEB_CLIENT_ID = process.env.GOOGLE_WEB_CLIENT_ID;
+export const BASE_URL = process.env.EXPO_PUBLIC_API_URL;
+export const GOOGLE_WEB_CLIENT_ID = process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID;
