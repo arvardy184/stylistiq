88643771dd9c46100c3e4f3c9d7fffb9c487b544
zakkiislamii <mzislami05@gmail.com>
2025-07-06 02:43:38 +0700

    add navbar, add logo, and add splash screen

---
+++ src/screens/collections/screen/screen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+const CollectionsScreen = () => (
+  <View className="flex-1 justify-center items-center bg-white">
+    <Text className="text-gray-500">Collections Screen - Coming Soon!</Text>
+  </View>
+);
+
+export default CollectionsScreen;
